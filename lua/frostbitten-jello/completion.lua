ab51be93edd887495e2fb012ee2ae76853954530
Frostbitten-jello <sanket.p.kumbhar.2805@gmail.com>
2022-12-29 14:25:22 +0530

    minor tweaks

--- lua/frostbitten-jello/completion.lua
+++ lua/frostbitten-jello/completion.lua
@@ -109,7 +109,7 @@ cmp.setup {
     mapping = cmp.mapping.preset.insert {
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-Space>'] = cmp.mapping.complete({}),
         ['<C-y>'] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
