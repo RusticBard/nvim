d06557fc204ec2f9476d7b0abdf8107e2e269d30
Frostbitten-jello <sanket.p.kumbhar.2805@gmail.com>
2022-12-30 00:36:44 +0530

    added navigation between diagnostics

--- lua/frostbitten-jello/completion.lua
+++ lua/frostbitten-jello/completion.lua
@@ -24,6 +24,8 @@ local on_attach = function(_, bufnr)
     nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
     nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
     nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+    nmap('<leader>dn', vim.diagnostic.goto_next, '[D]iagnostic [N]ext')
+    nmap('<leader>dp', vim.diagnostic.goto_prev, '[D]iagnostic [P]revious')
 
     -- See `:help K` for why this keymap
     nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
