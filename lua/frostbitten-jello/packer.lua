214b2ee0d777ff5ecc1a05f27ed610609e8e9bc4
Frostbitten-jello <sanket.p.kumbhar.2805@gmail.com>
2022-12-30 19:26:49 +0530

    fidget-nvimtree fix

--- lua/frostbitten-jello/packer.lua
+++ lua/frostbitten-jello/packer.lua
@@ -34,7 +34,7 @@ return require('packer').startup({function(use)
         requires = {
             'nvim-tree/nvim-web-devicons', -- optional, for file icons
         },
-        tag = 'nightly' -- optional, updated every week. (see issue #1193)
+        -- tag = 'nightly' -- optional, updated every week. (see issue #1193)
     }
 
     -- Eyecandy
