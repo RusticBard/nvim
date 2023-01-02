fc8b11ea8ed06f933bd1e8e58349f5e8cc335b5f
Frostbitten-jello <sanket.p.kumbhar.2805@gmail.com>
2023-01-02 21:34:04 +0530

    minor tweaks

--- lua/frostbitten-jello/telescope.lua
+++ lua/frostbitten-jello/telescope.lua
@@ -6,28 +6,19 @@ require('telescope').setup {
             },
         },
     },
-    extensions = {
-        undo = {
-            -- telescope-undo.nvim config
-            width = 1,
-            use_delta = true,
-            use_custom_command = nil, -- setting this implies `use_delta = false`. Accepted format is: { "bash", "-c", "echo '$DIFF' | delta" }
-            side_by_side = false,
-            diff_context_lines = vim.o.scrolloff,
-            entry_format = "state #$ID, $STAT, $TIME",
-            mappings = {
-                i = {
-                    -- IMPORTANT: Note that telescope-undo must be available when telescope is configured if
-                    -- you want to replicate these defaults and use the following actions. This means
-                    -- installing as a dependency of telescope in it's `requirements` and loading this
-                    -- extension from there instead of having the separate plugin definition as outlined
-                    -- above.
-                    ["<cr>"] = require("telescope-undo.actions").yank_additions,
-                    ["<S-cr>"] = require("telescope-undo.actions").yank_deletions,
-                    ["<C-cr>"] = require("telescope-undo.actions").restore,
-                },
-            },
+    pickers = {
+        find_files = {
+            theme = 'dropdown',
+            previewer = false,
+            width = 2,
         },
+        buffers = {
+            theme = 'dropdown',
+            previewer = false,
+            width = 2,
+        },
+    },
+    extensions = {
     },
 }
 
@@ -51,8 +42,7 @@ vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sc', '<cmd>Telescope find_files cwd=~/.config<CR>' , { desc = '[S]earch [F]iles' })
 
 -- telescope-undo keymaps 
-vim.keymap.set("n", "<leader>ut", "<cmd>Telescope undo<cr>")
+vim.keymap.set("n", "<leader>ut", require("telescope").extensions.undo.undo)
