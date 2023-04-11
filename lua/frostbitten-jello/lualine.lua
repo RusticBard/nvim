8f58a08528b1bf1727bfc0bd9d253c0682b2d384
Frostbitten-jello <sanket.p.kumbhar.2805@gmail.com>
2023-04-11 21:50:29 +0530

    Added bubbles theme for lualine

--- lua/frostbitten-jello/lualine.lua
+++ lua/frostbitten-jello/lualine.lua
@@ -1,40 +1,9 @@
 -- Bubbles config for lualine
--- Author: lokesh-krishna
--- MIT license, see LICENSE for more details.
-
--- stylua: ignore
-local colors = {
-  blue   = '#80a0ff',
-  cyan   = '#79dac8',
-  black  = '#1E1E1E', --'#080808',
-  white  = '#c6c6c6',
-  red    = '#ff5189',
-  violet = '#d183e8',
-  grey   = '#303030',
-}
-
-local bubbles_theme = {
-  normal = {
-    a = { fg = colors.black, bg = colors.violet },
-    b = { fg = colors.white, bg = colors.grey },
-    c = { fg = colors.black, bg = colors.black },
-  },
-
-  insert = { a = { fg = colors.black, bg = colors.blue } },
-  visual = { a = { fg = colors.black, bg = colors.cyan } },
-  replace = { a = { fg = colors.black, bg = colors.red } },
-
-  inactive = {
-    a = { fg = colors.white, bg = colors.black },
-    b = { fg = colors.white, bg = colors.black },
-    c = { fg = colors.black, bg = colors.black },
-  },
-}
 
 require('lualine').setup {
   options = {
-    theme = 'auto', --bubbles_theme,
-    component_separators = '|',
+    theme = 'auto',
+    component_separators = '│',
     section_separators = { left = '', right = '' },
   },
   sections = {
