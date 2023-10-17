11d4732595eab3f02e8c174783d0a9f6ae52b75d
frostbitten-jello <sanket.p.kumbhar.2805@gmail.com>
2023-10-18 01:35:29 +0530

    new start

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,19 +1,35 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
+  main = 'ibl',
   config = function()
     -- fancy characters
     vim.opt.list = true
     --vim.opt.listchars:append 'space:⋅'
     vim.opt.listchars:append 'eol:↴'
+    local highlight = {
+      -- "CursorColumn",
+      "Whitespace",
+    }
 
-    require('indent_blankline').setup {
+    require('ibl').setup {
       enabled = true,
-      colored_indent_levels = true,
-      char = '¦', -- ┊┆ ⎸
-      show_trailing_blankline_indent = false,
-      space_char_blankline = ' ',
-      show_current_context = true,
-      show_current_context_start = false,
+      -- colored_indent_levels = true,
+      -- show_trailing_blankline_indent = false,
+      -- space_char_blankline = ' ',
+      -- show_current_context = true,
+      -- show_current_context_start = false,
+      indent = {
+        highlight = highlight,
+        char = "▏" -- ¦┊┆ ⎸
+      },
+      scope = {
+        enabled = true,
+        show_start = true,
+        show_end = false,
+        injected_languages = false,
+        highlight = { "Function", "Label" },
+        priority = 500,
+      },
     }
   end,
 }
