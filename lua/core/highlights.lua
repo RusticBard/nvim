88738162a63d47b382eacc7d5bc938f41a7e90e2
RusticBard <sanket.p.kumbhar.2805@gmail.com>
2025-12-30 16:11:51 +0530

    Changed transparency

---
+++ lua/core/highlights.lua
@@ -0,0 +1,9 @@
+vim.cmd("colorscheme carbonfox")
+
+-- local pmenu_color = vim.api.nvim_get_hl(0, { name = "PmenuSel" }).bg
+
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "CursorLine", { bg = "#525252" })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#181718" })
+-- vim.api.nvim_set_hl(0, "Visual", { link = "PMenuSel" })
+-- vim.api.nvim_set_hl(0, "Whitespace", { fg = pmenu_color, bg = "NONE" })
